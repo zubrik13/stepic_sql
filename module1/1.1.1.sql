95a1fda91eff2cd8f5cd218581aab84a27529d6c
zubrik <rzubrytski@gmail.com>
2021-01-11 02:43:18 +0300

    initial setup

---
+++ module1/1.1.1.sql
@@ -0,0 +1,9 @@
+/*create table*/
+create table book (book_id INT PRIMARY KEY AUTO_INCREMENT,
+                    title VARCHAR(50),
+                    author VARCHAR(30),
+                    price DECIMAL(8, 2),
+                    amount INT);
+
+
+show tables;
---
+++ module1/1.1.2.sql
@@ -0,0 +1,44 @@
+/*
+INT PRIMARY KEY AUTO_INCREMENT	VARCHAR(50)	VARCHAR(30)	DECIMAL(8,2)	INT
+1	Мастер и Маргарита	Булгаков М.А.	670.99	3
+2	Белая гвардия	Булгаков М.А.	540.50	5
+3	Идиот	Достоевский Ф.М.	460.00	10
+4	Братья Карамазовы	Достоевский Ф.М.	799.01	2
+*/
+
+
+insert
+into
+    book
+    (title, author, price, amount)
+values
+    ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3);
+
+
+insert
+into
+    book
+    (title, author, price, amount)
+values
+    ('Белая гвардия', 'Булгаков М.А.', 540.50, 5);
+
+
+insert
+into
+    book
+    (title, author, price, amount)
+values
+    ('Идиот', 'Достоевский Ф.М.', 460.00, 10);
+
+
+insert
+into
+    book
+    (title, author, price, amount)
+values
+    ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2);
+
+select * from book;
+
+
+
